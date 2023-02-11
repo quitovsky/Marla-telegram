78c0f662b84aebd02c9296b82d4db0ff60cfd3f6
quitovsky <quitovsky@vk.com>
2023-02-11 23:52:03 +0300

    Created base for frontend.

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import css from '@/styles/index.module.scss'
+
+export default function () {
+	return(<>
+		<div className={css.center}>
+			<div className={css.title}>Marla</div>
+			<div className={css.subtitle}>
+				«If I did have a tumor, I'd name it Marla.»
+			</div>
+		</div>
+		</>)
+}
