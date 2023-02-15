62e2753e21e83c32a925f0920e4d4ee20158a8c0
quitovsky <110604826+quitovsky@users.noreply.github.com>
2023-02-15 21:01:35 +0300

    Feat/chat records (#4)
    * Created base recordings of chats into database.
    * Moved from sqlite3 to postgres.
    * Removed some unnecessary things.

--- bot/middlewares/main.ts
+++ bot/middlewares/main.ts
@@ -1,13 +1,23 @@
 import {MarlaContext} from "../utils/types/context";
 import {NextFunction} from "grammy";
-import {Message} from "../utils/entities/message";
+import prisma from "../lib/prisma";
 
 export async function marlaMiddleware(ctx: MarlaContext, next: NextFunction) {
 	ctx.isPrivate = ctx.chat?.id === ctx.message?.from.id
-	if (ctx.message?.text) {
-		ctx.m = new Message(ctx.message.text, ctx.message.from.first_name, ctx)
-	} else {
-		ctx.m = new Message("", "", ctx)
+	if(ctx.message && ctx.chat && !ctx.isPrivate) {
+		const count = await prisma.chat.count({
+			where: {
+				chatId: ctx.chat.id
+			}
+		})
+		if (count === 0) {
+			console.log('Registration')
+			await prisma.chat.create({
+				data: {
+					chatId: ctx.chat.id
+				}
+			})
+		}
 	}
 	await next()
 }
