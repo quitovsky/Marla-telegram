7bb412f2369d5eddf296b4902a85ed9344cabd02
quitovsky <quitovsky@vk.com>
2023-02-19 11:44:05 +0300

    Feat: change privacy of the chat(!privacy)

--- bot/plugins/publicChatsLink.ts
+++ bot/plugins/publicChatsLink.ts
@@ -5,7 +5,7 @@ import prisma from "../lib/prisma";
 
 const composer = new Composer<MarlaContext>()
 
-composer.hears(/!(link)|(линк)|(ссылка)|(123)/i, async ctx => {
+composer.hears(/!(link)|(линк)|(ссылка)/i, async ctx => {
     if(ctx.isPrivate) return ctx.reply(onlyForChats);
     const chat = await prisma.chat.findFirst({
         where: {
@@ -18,9 +18,40 @@ composer.hears(/!(link)|(линк)|(ссылка)|(123)/i, async ctx => {
     })
     if(!chat) return await ctx.reply(somethingWentWrong)
     if(chat.public) {
-        return await ctx.reply(`.../${chat.id}`)
+        return await ctx.reply(`https://marla.su/chat/${chat.id}`)
     } else {
         return await ctx.reply(onlyForPublicChats)
+    }
+})
+
+composer.hears(/!(приватность)|(закрыть)|(открыть)|(приватность)|(privacy)/i, async ctx => {
+    if(ctx.isPrivate) return ctx.reply(onlyForChats);
+    const chat = await prisma.chat.findFirst({
+        where: {
+            chatId: ctx.chat.id
+        }
+    })
+    if(!chat) return await ctx.reply(somethingWentWrong)
+    if(chat.public) {
+        await prisma.chat.update({
+            where: {
+                chatId: ctx.chat.id
+            },
+            data: {
+                public: false
+            }
+        })
+        await ctx.reply("Чат теперь приватный")
+    } else {
+        await prisma.chat.update({
+            where: {
+                chatId: ctx.chat.id
+            },
+            data: {
+                public: true
+            }
+        })
+        await ctx.reply("Чат теперь публичный")
     }
 })
 
