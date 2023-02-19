dbc54952ac338818bfce7e9c2a09b73e0464db0f
quitovsky <quitovsky@vk.com>
2023-02-19 12:20:44 +0300

    changed api port (for prod)

--- frontend/pages/api/chat/[id].ts
+++ frontend/pages/api/chat/[id].ts
@@ -2,6 +2,6 @@ import {NextApiRequest, NextApiResponse} from "next";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
     const host = process.env.NEXTT_PUBLIC_HOST || "https://marla.su"
-    const data: any = await fetch(`${host}:5000/chat/${req.query.id}`).then(res => res.json())
+    const data: any = await fetch(`${host}:2087/chat/${req.query.id}`).then(res => res.json())
     res.status(200).send(data)
 }
